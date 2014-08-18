b0b6b125d6265e0237767ad941aea462f26d06d3
Olafur Waage <olafurw@gmail.com>
2014-08-18 23:11:23 +0200

    Minor indentation and build fixes.

--- card.cpp
+++ card.cpp
@@ -16,13 +16,11 @@ card::card(const std::string& name,
 	target(target::type::card),
 	m_mana(mana), m_damage(damage),
 	m_health(health), m_max_health(health),
-	m_attack_count((m_windfury ? 2 : 1)),
-	m_max_attack_count(m_attack_count),
-    m_shield(shield),
-    m_charge(charge),
-    m_taunt(taunt),
-    m_attack(charge),
-    m_awake(charge),
+	m_shield(shield),
+	m_charge(charge),
+	m_taunt(taunt),
+	m_attack(charge),
+	m_awake(charge),
 	m_alive(true),
 	m_windfury(windfury),
 	m_placed(false),
@@ -30,6 +28,9 @@ card::card(const std::string& name,
 	m_name(name)
 {
 	m_id = static_cast<unsigned int>(random_generator()());
+	
+	m_attack_count = (m_windfury ? 2 : 1);
+	m_max_attack_count = m_attack_count;
 }
 
 card::card(const card& c): target(target::type::card)
@@ -75,77 +76,77 @@ card::card(card&& c): target(target::type::card)
 	m_alive = c.m_alive;
 	m_awake = c.m_awake;
 	m_windfury = c.m_windfury;
-    m_attack_count = c.m_attack_count;
-    m_max_attack_count = c.m_max_attack_count;
+	m_attack_count = c.m_attack_count;
+	m_max_attack_count = c.m_max_attack_count;
 
-    m_battle_cry = std::move(c.m_battle_cry);
+	m_battle_cry = std::move(c.m_battle_cry);
 
-    m_placed = c.m_placed;
-    m_destroy = c.m_destroy;
+	m_placed = c.m_placed;
+	m_destroy = c.m_destroy;
 }
 
 card& card::operator =(const card& c)
 {
-	if(this != &c)
+	if(this == &c)
 	{
-	    m_type = c.get_type();
-		m_id = c.m_id;
-		m_name = c.m_name;
-
-		m_mana = c.m_mana;
-		m_damage = c.m_damage;
-		m_health = c.m_health;
-		m_max_health = c.m_max_health;
-
-		m_shield = c.m_shield;
-		m_taunt = c.m_taunt;
-		m_charge = c.m_charge;
-		m_attack = c.m_attack;
-		m_alive = c.m_alive;
-		m_awake = c.m_awake;
-		m_windfury = c.m_windfury;
-	    m_attack_count = c.m_attack_count;
-	    m_max_attack_count = c.m_max_attack_count;
-
-	    m_battle_cry = c.m_battle_cry;
-
-	    m_placed = c.m_placed;
-	    m_destroy = c.m_destroy;
+		return *this;
 	}
+	
+	m_type = c.get_type();
+	m_id = c.m_id;
+	m_name = c.m_name;
 
-	return *this;
+	m_mana = c.m_mana;
+	m_damage = c.m_damage;
+	m_health = c.m_health;
+	m_max_health = c.m_max_health;
+
+	m_shield = c.m_shield;
+	m_taunt = c.m_taunt;
+	m_charge = c.m_charge;
+	m_attack = c.m_attack;
+	m_alive = c.m_alive;
+	m_awake = c.m_awake;
+	m_windfury = c.m_windfury;
+	m_attack_count = c.m_attack_count;
+	m_max_attack_count = c.m_max_attack_count;
+
+	m_battle_cry = c.m_battle_cry;
+
+	m_placed = c.m_placed;
+	m_destroy = c.m_destroy;
 }
 
 card& card::operator =(card&& c)
 {
-	if(this != &c)
+	if(this == &c)
 	{
-	    m_type = c.get_type();
-		m_id = c.m_id;
-		m_name = std::move(c.m_name);
-
-		m_mana = c.m_mana;
-		m_damage = c.m_damage;
-		m_health = c.m_health;
-		m_max_health = c.m_max_health;
-
-		m_shield = c.m_shield;
-		m_taunt = c.m_taunt;
-		m_charge = c.m_charge;
-		m_attack = c.m_attack;
-		m_alive = c.m_alive;
-		m_awake = c.m_awake;
-		m_windfury = c.m_windfury;
-	    m_attack_count = c.m_attack_count;
-	    m_max_attack_count = c.m_max_attack_count;
-
-	    m_battle_cry = std::move(c.m_battle_cry);
-
-	    m_placed = c.m_placed;
-	    m_destroy = c.m_destroy;
+		return *this;
 	}
 
-	return *this;
+	m_type = c.get_type();
+	m_id = c.m_id;
+	m_name = std::move(c.m_name);
+
+	m_mana = c.m_mana;
+	m_damage = c.m_damage;
+	m_health = c.m_health;
+	m_max_health = c.m_max_health;
+
+	m_shield = c.m_shield;
+	m_taunt = c.m_taunt;
+	m_charge = c.m_charge;
+	m_attack = c.m_attack;
+	m_alive = c.m_alive;
+	m_awake = c.m_awake;
+	m_windfury = c.m_windfury;
+	m_attack_count = c.m_attack_count;
+	m_max_attack_count = c.m_max_attack_count;
+
+	m_battle_cry = std::move(c.m_battle_cry);
+
+	m_placed = c.m_placed;
+	m_destroy = c.m_destroy;
 }
 
 void card::attack(card& c)
@@ -171,13 +172,13 @@ void card::attack(player* p)
 	{
 		p->take_damage(m_damage);
 
-        --m_attack_count;
+		--m_attack_count;
 
-        // Cannot attack again this round until refreshed
-        if(m_attack_count == 0)
-        {
-            m_attack = false;
-        }
+		// Cannot attack again this round until refreshed
+		if(m_attack_count == 0)
+		{
+			m_attack = false;
+		}
 	}
 }
 
@@ -190,12 +191,12 @@ void card::refresh()
 
 void card::take_damage(int damage)
 {
-    if(m_shield)
-    {
-        m_shield = false;
+	if(m_shield)
+	{
+		m_shield = false;
 
-        return;
-    }
+		return;
+	}
 
 	// We do not want health to go below zero.
 	// Here the card dies.
@@ -212,20 +213,20 @@ void card::take_damage(int damage)
 
 void card::give_health(int health)
 {
-    if(!m_alive)
-    {
-        return;
-    }
+	if(!m_alive)
+	{
+		return;
+	}
 
-    // We can not go over max health
-    if(health + m_health > m_max_health)
-    {
-        m_health = m_max_health;
+	// We can not go over max health
+	if(health + m_health > m_max_health)
+	{
+		m_health = m_max_health;
 
-        return;
-    }
+		return;
+	}
 
-    m_health += health;
+	m_health += health;
 }
 
 bool card::can_attack() const
@@ -245,22 +246,22 @@ bool card::awake() const
 
 bool card::placed() const
 {
-    return m_placed;
+	return m_placed;
 }
 
 void card::set_placed(bool p)
 {
-    m_placed = p;
+	m_placed = p;
 }
 
 bool card::destroy() const
 {
-    return m_destroy;
+	return m_destroy;
 }
 
 void card::set_destroy(bool d)
 {
-    m_destroy = d;
+	m_destroy = d;
 }
 
 std::string card::name() const
@@ -270,12 +271,12 @@ std::string card::name() const
 
 bool card::shield() const
 {
-    return m_shield;
+	return m_shield;
 }
 
 void card::give_shield()
 {
-    m_shield = true;
+	m_shield = true;
 }
 
 bool card::taunt() const
@@ -285,29 +286,29 @@ bool card::taunt() const
 
 void card::give_taunt()
 {
-    m_taunt = true;
+	m_taunt = true;
 }
 
 bool card::charge() const
 {
-    return m_charge;
+	return m_charge;
 }
 
 void card::give_charge()
 {
-    m_charge = true;
+	m_charge = true;
 }
 
 bool card::windfury() const
 {
-    return m_windfury;
+	return m_windfury;
 }
 
 void card::give_windfury()
 {
-    m_windfury = true;
-    m_attack_count = 2;
-    m_max_attack_count = 2;
+	m_windfury = true;
+	m_attack_count = 2;
+	m_max_attack_count = 2;
 }
 
 int card::mana() const
@@ -317,54 +318,54 @@ int card::mana() const
 
 int card::damage() const
 {
-    return m_damage;
+	return m_damage;
 }
 
 int card::health() const
 {
-    return m_health;
+	return m_health;
 }
 
 void card::add_battle_cry(std::shared_ptr<effect> e)
 {
-    m_battle_cry.push_back(e);
+	m_battle_cry.push_back(e);
 }
 
 void card::process_battle_cry()
 {
-    for(auto& effect : m_battle_cry)
-    {
-        effect->set_parent(this);
-        effect->process();
-    }
+	for(auto& effect : m_battle_cry)
+	{
+		effect->set_parent(this);
+		effect->process();
+	}
 }
 
 void card::add_on_going(std::shared_ptr<effect> e)
 {
-    m_on_going.push_back(e);
+	m_on_going.push_back(e);
 }
 
 void card::process_on_going()
 {
-    for(auto& effect : m_on_going)
-    {
-        effect->set_parent(this);
-        effect->process();
-    }
+	for(auto& effect : m_on_going)
+	{
+		effect->set_parent(this);
+		effect->process();
+	}
 }
 
 void card::add_death_rattle(std::shared_ptr<effect> e)
 {
-    m_death_rattle.push_back(e);
+	m_death_rattle.push_back(e);
 }
 
 void card::process_death_rattle()
 {
-    for(auto& effect : m_death_rattle)
-    {
-        effect->set_parent(this);
-        effect->process();
-    }
+	for(auto& effect : m_death_rattle)
+	{
+		effect->set_parent(this);
+		effect->process();
+	}
 }
 
 bool card::operator ==(const card& c)
